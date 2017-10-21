95738e550dd7d0d3400dd5dab95092c93d910da8
jonathan Morton <jmorton4193@gmail.com>
2017-10-21 21:22:57 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init(e) {
   const key = parseInt(e.detail || e.which);
-
+  let index=0; 
   if (key === code[index]) {
     index++;
 
@@ -17,6 +17,6 @@ function init(e) {
 
 }
 
-let index = 0;
+
 
 // This is the function that would be invoked by the event listener.
