6d7065d6db7a190b28b86b6223b943f44358d5ef
jonathan Morton <jmorton4193@gmail.com>
2017-10-21 21:38:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,8 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-const v = document.body.addEventListener('keydown', function init(e) {
+let index=0;
+ function init() {
+document.body.addEventListener('keydown', function(e){
   const key = parseInt(e.detail || e.which);
-  let index=0;
   if (key === code[index]) {
     index++;
 
@@ -12,7 +13,7 @@ const v = document.body.addEventListener('keydown', function init(e) {
     }
   } else {
     index = 0;
-  }
+  })
 // Write your JavaScript code inside the init() function
 
 }
