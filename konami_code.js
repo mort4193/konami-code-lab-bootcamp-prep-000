ac57b7a17b9f2566e57f7247a11e36d2d2e8256a
jonathan Morton <jmorton4193@gmail.com>
2017-10-21 21:23:05 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init(e) {
   const key = parseInt(e.detail || e.which);
-  let index=0; 
+  let index=0;
   if (key === code[index]) {
     index++;
 
